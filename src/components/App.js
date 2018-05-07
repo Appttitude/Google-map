15557cabc377e26e42b1c3fb56a6bd2b03860fb4
Esteban Ortiz <ematiasortiz@gmail.com>
2018-05-07 11:42:31 -0300

    geolocation, get lat and lng

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Location from "./maps/ViewMap";
+
+class App extends Component {
+  render() {
+    return <Location />;
+  }
+}
+
+export default App;
---
+++ src/components/maps/ViewMap.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Geolocation from "react-geolocation";
+
+class ViewMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: "",
+      lng: "",
+      success: false
+    };
+
+    this.handleSuccess = this.handleSuccess.bind(this);
+  }
+
+  handleSuccess() {
+    this.setState({ success: !this.state.success });
+  }
+
+  render() {
+    const { error, success } = this.state;
+    if (error) {
+      return <h1>{error}</h1>;
+    }
+    return (
+      <Geolocation
+        lazy
+        onSuccess={this.handleSuccess}
+        render={({
+          fetchingPosition,
+          position: { coords: { latitude, longitude } = {} } = {},
+          error,
+          getCurrentPosition
+        }) => {
+          if (fetchingPosition) return <h1>FETCHING POSITION</h1>;
+          return (
+            <div>
+              <button onClick={getCurrentPosition}>Get Position</button>
+              {error && <div>{error.message}</div>}
+              {success && (
+                <div>
+                  latitud= {latitude}
+                  longitud={longitude}
+                </div>
+              )}
+            </div>
+          );
+        }}
+      />
+    );
+  }
+}
+
+export default ViewMap;
