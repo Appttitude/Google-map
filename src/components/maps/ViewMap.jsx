18cd7622322b43e8c0169cc45b4b40d316b24caa
Esteban Ortiz <ematiasortiz@gmail.com>
2018-05-07 23:53:54 -0300

    load map

--- src/components/maps/ViewMap.jsx
+++ src/components/maps/ViewMap.jsx
@@ -13,38 +13,13 @@ class ViewMap extends Component {
     this.handleSuccess = this.handleSuccess.bind(this);
   }
 
-  /*
-  geocodeLatLng(geocoder, map, infowindow) {
-    var latlngStr = input.split(",", 2);
-    var latlng = {
-      lat: parseFloat(latlngStr[0]),
-      lng: parseFloat(latlngStr[1])
-    };
-    geocoder.geocode({ location: latlng }, function(results, status) {
-      if (status === "OK") {
-        if (results[1]) {
-          infowindow.setContent(results[1].formatted_address);
-          infowindow.open(map, marker);
-        } else {
-          window.alert("No results found");
-        }
-      } else {
-        window.alert("Geocoder failed due to: " + status);
-      }
-    });
-  }*/
-
   handleSuccess(success) {
-    // const geocoder = new google.maps.Geocoder();
-    //const infowindow = new google.maps.InfoWindow();
-    //console.log("handleSuccess");
     this.setState({
       success
     });
   }
 
   render() {
-    //console.log(this.state.success);
     const { error, success } = this.state;
 
     if (error) {
