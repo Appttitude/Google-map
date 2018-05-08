06edaf007fc47d7802d29b6324e797b01e805db3
Esteban Ortiz <ematiasortiz@gmail.com>
2018-05-08 03:06:39 -0300

    end get position (country and city)

--- src/components/maps/ViewMap.jsx
+++ src/components/maps/ViewMap.jsx
@@ -2,25 +2,51 @@ import React, { Component } from "react";
 import Geolocation from "react-geolocation";
 import Map from "./MyMapComponent";
 import img from "./loading-map.gif";
+import googleMapsClient from "@google/maps";
 
 class ViewMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      success: false
+      country: null
     };
-
+    this.handleError = this.handleError.bind(this);
     this.handleSuccess = this.handleSuccess.bind(this);
   }
 
-  handleSuccess(success) {
-    this.setState({
-      success
+  handleError(error) {
+    this.setState({ error: error.message });
+  }
+
+  handleSuccess(position) {
+    const gmc = googleMapsClient.createClient({
+      key: "AIzaSyCBjKHxpTGLEg3F5cCP95xwJyxxTZAvtTo",
+      Promise: Promise
     });
+    gmc
+      .reverseGeocode({
+        latlng: `${position.coords.latitude},${position.coords.longitude}`
+      })
+      .asPromise()
+      .then(response => {
+        let country = "",
+          fullCity = "";
+        response.json.results.forEach(result => {
+          if (result.types.includes("country"))
+            country = result.formatted_address;
+          if (result.types.includes("administrative_area_level_2"))
+            fullCity = result.formatted_address;
+        });
+
+        this.setState({ country, fullCity });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
   }
 
   render() {
-    const { error, success } = this.state;
+    const { fullCity, country, error } = this.state;
 
     if (error) {
       return <h1>{error}</h1>;
@@ -29,6 +55,7 @@ class ViewMap extends Component {
     return (
       <Geolocation
         lazy
+        onError={this.handleError}
         onSuccess={this.handleSuccess}
         render={({
           fetchingPosition,
@@ -41,8 +68,11 @@ class ViewMap extends Component {
             <div>
               <button onClick={getCurrentPosition}>Get Position</button>
               {error && <div>{error.message}</div>}
-              {success && (
+              {country && (
                 <div>
+                  <h3>Country:{country}</h3>
+                  <h4>City:{fullCity.split(",").shift()}</h4>
+                  {/*shift toma primer valor del arreglo*/}
                   <Map lat={latitude} lng={longitude} isMarkerShown />
                 </div>
               )}
