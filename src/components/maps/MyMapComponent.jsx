b76b7c004cc19a6f8befc7ccdc4279424d4bf856
Esteban Ortiz <ematiasortiz@gmail.com>
2018-05-08 12:09:27 -0300

    error in map render

--- src/components/maps/MyMapComponent.jsx
+++ src/components/maps/MyMapComponent.jsx
@@ -10,12 +10,6 @@ import PropTypes from "prop-types";
 
 const MyMapComponent = compose(
   withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
-
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyCBjKHxpTGLEg3F5cCP95xwJyxxTZAvtTo&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
@@ -30,7 +24,13 @@ const MyMapComponent = compose(
     defaultCenter={{ lat: props.lat, lng: props.lng }}
   >
     {props.isMarkerShown && (
-      <Marker position={{ lat: props.lat, lng: props.lng }} />
+      <Marker
+        position={{
+          lat:
+            typeof props.lat === "number" ? props.lat : parseFloat(props.lat),
+          lng: typeof props.lng === "number" ? props.lng : parseFloat(props.lng)
+        }}
+      />
     )}
   </GoogleMap>
 ));
--- src/components/maps/ViewMap.jsx
+++ src/components/maps/ViewMap.jsx
@@ -4,6 +4,16 @@ import Map from "./MyMapComponent";
 import img from "./loading-map.gif";
 import googleMapsClient from "@google/maps";
 
+const DetailMap = ({ latitude, longitude, country, fullCity }) => (
+  <div>
+    <h3>Country:{country}</h3>
+    <h4>City:{fullCity.split(",").shift()}</h4>
+    {/*shift toma primer valor del arreglo*/}
+
+    <Map lat={latitude} lng={longitude} isMarkerShown />
+  </div>
+);
+
 class ViewMap extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +22,16 @@ class ViewMap extends Component {
     };
     this.handleError = this.handleError.bind(this);
     this.handleSuccess = this.handleSuccess.bind(this);
+    this.renderMap = this.renderMap.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //const lat = this.state.latitude;
+    //debugger;
+    //console.log(nextState.country);
+    console.log(nextState);
+    // console.log(typeof nextState.latitude);
+    return nextState.country !== this.state.country;
   }
 
   handleError(error) {
@@ -45,40 +65,41 @@ class ViewMap extends Component {
       });
   }
 
-  render() {
-    const { fullCity, country, error } = this.state;
-
+  renderMap({
+    fetchingPosition,
+    position: { coords: { latitude, longitude } = {} } = {},
+    error,
+    getCurrentPosition
+  }) {
+    console.log(latitude);
+    const { fullCity, country } = this.state;
     if (error) {
       return <h1>{error}</h1>;
     }
+    if (fetchingPosition) return <img src={img} alt={"img"} />;
+    return (
+      <div>
+        <button onClick={getCurrentPosition}>Get Position</button>
+        {error && <div>{error.message}</div>}
+        {country && (
+          <DetailMap
+            latitude={latitude}
+            longitude={longitude}
+            country={country}
+            fullCity={fullCity}
+          />
+        )}
+      </div>
+    );
+  }
 
+  render() {
     return (
       <Geolocation
         lazy
         onError={this.handleError}
         onSuccess={this.handleSuccess}
-        render={({
-          fetchingPosition,
-          position: { coords: { latitude, longitude } = {} } = {},
-          error,
-          getCurrentPosition
-        }) => {
-          if (fetchingPosition) return <img src={img} alt={"img"} />;
-          return (
-            <div>
-              <button onClick={getCurrentPosition}>Get Position</button>
-              {error && <div>{error.message}</div>}
-              {country && (
-                <div>
-                  <h3>Country:{country}</h3>
-                  <h4>City:{fullCity.split(",").shift()}</h4>
-                  {/*shift toma primer valor del arreglo*/}
-                  <Map lat={latitude} lng={longitude} isMarkerShown />
-                </div>
-              )}
-            </div>
-          );
-        }}
+        render={this.renderMap}
       />
     );
   }
